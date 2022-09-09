881c4568504455489b33326d96bf7d9475460e60
顶峰云雪 <pinus0716@163.com>
2022-09-09 14:28:21 +0800

    初步更新

---
+++ 面经实践/JS/this.js
@@ -0,0 +1,42 @@
+// var id = "Global";
+
+// function fun1() {
+// 	// setTimeout中使用普通函数
+// 	setTimeout(function () {
+// 		console.log(this.id);
+// 	}, 2000);
+// }
+
+// function fun2() {
+// 	// setTimeout中使用箭头函数
+// 	setTimeout(() => {
+// 		console.log(this.id);
+// 	}, 2000);
+// }
+
+// fun1.call({ id: "Obj" }); // 'Global'
+
+// fun2.call({ id: "Obj" }); // 'Obj'
+
+// 例子一
+let fun = (val) => {
+	console.log(val); // 111
+	// 下面一行会报错
+	// Uncaught ReferenceError: arguments is not defined
+	// 因为外层全局环境没有arguments对象
+	console.log(arguments);
+};
+fun(111);
+
+// 例子二
+function outer(val1, val2) {
+	let argOut = arguments;
+	console.log(argOut); // ①
+	let fun = () => {
+		let argIn = arguments;
+		console.log(argIn); // ②
+		console.log(argOut === argIn); // ③
+	};
+	fun();
+}
+outer(111, 222);
