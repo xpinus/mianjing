85e1cf6e7038f88444a86bc74c6d406fcfb4d20c
顶峰云雪 <pinus0716@163.com>
2022-10-12 13:09:09 +0800

    更新

--- docs/React/4 对react和vue的理解.md
+++ docs/React/4 对react和vue的理解.md
@@ -3,3 +3,5 @@ sort: 4
 ---
 
 # 对react和vue的理解
+
+https://juejin.cn/post/6844904158093377549
--- 面经实践/JS/map.js
+++ 面经实践/JS/map.js
@@ -3,3 +3,5 @@ const arr = [1, 2, 3, undefined, 4, undefined, null];
 arr.map((item) => {
 	console.log(item);
 });
+
+arr.forEach((item) => console.log(item));
